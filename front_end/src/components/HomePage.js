255a23e0e5c40c491d00f1c9a5e52c3fb74662bb
aledrinyth <58565713+aledrinyth@users.noreply.github.com>
2023-08-27 16:24:59 +1000

    Add files via upload

--- front_end/src/components/HomePage.js
+++ front_end/src/components/HomePage.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import { Link, Navigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const HomePage = () => {
 
@@ -22,58 +24,58 @@ const HomePage = () => {
   };
 
   if (redirectToNewPage) {
-    return <Navigate to="/add" />;
+    return <Navigate to="/add-transaction" />;
   }
 
   return (
     <div>
       {/* <h1>This is the home page!</h1>
       <li>
-        <Link to="/add">Add a transaction</Link>
+        <Link to="/add-transaction">Add a transaction</Link>
       </li> */}
 
-      <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="/#">WiseWallet</a>
-          <a class="navbar-brand justify-content-end" href="#">
-            <img src="https://png.pngtree.com/png-vector/20190704/ourmid/pngtree-businessman-user-avatar-free-vector-png-image_1538405.jpg" alt="Logo" style={{width:40}} class="rounded-pill"/>
+      <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/#">WiseWallet</a>
+          <a className="navbar-brand justify-content-end" href="#">
+            <img src="https://png.pngtree.com/png-vector/20190704/ourmid/pngtree-businessman-user-avatar-free-vector-png-image_1538405.jpg" alt="Logo" style={{width:40}} className="rounded-pill"/>
           </a>
         </div>
       </nav>
 
       <h1 style={headingStyle}>August 2023</h1>
       
-      <div class="row">
-        <div class="col-sm-4 p-4 my-4 border d-flex flex-column align-items-center">
-            <h2 class="text-center mb-4">Overview</h2>
-            <div class="circle d-flex justify-content-center align-items-center" style={circleStyle}>
-              <div class="text-center">
-                <p class="mb-1">Net Savings</p>
-                <h4 class="mb-0">-$101.04</h4>
+      <div className="row">
+        <div className="col-sm-4 p-4 my-4 border d-flex flex-column align-items-center">
+            <h2 className="text-center mb-4">Overview</h2>
+            <div className="circle d-flex justify-content-center align-items-center" style={circleStyle}>
+              <div className="text-center">
+                <p className="mb-1">Net Savings</p>
+                <h4 className="mb-0">-$101.04</h4>
               </div>
             </div>
         </div>
-        <div class="col-sm-8 bg-secondary p-4 my-4 border">
+        <div className="col-sm-8 bg-secondary p-4 my-4 border">
           <h3>Net Savings for the Year</h3>
         </div>
       </div>
 
-      <div class="container mt-4">
-        <h2 class="text-center">Categories</h2>
+      <div className="container mt-4">
+        <h2 className="text-center">Categories</h2>
         <br/>
-        <div class="d-flex justify-content-center">
-          <div class="btn-group" role="group">
-            <button type="button" class="btn btn-outline-secondary btn-lg">Food</button>
-            <button type="button" class="btn btn-outline-secondary btn-lg">Bills</button>
-            <button type="button" class="btn btn-outline-secondary btn-lg">Transport</button>
-            <button type="button" class="btn btn-outline-secondary btn-lg">Healthcare</button>
-            <button type="button" class="btn btn-outline-secondary btn-lg">House</button>
-            <button type="button" class="btn btn-outline-secondary btn-lg">Savings</button>
+        <div className="d-flex justify-content-center">
+          <div className="btn-group" role="group">
+            <button type="button" className="btn btn-outline-secondary btn-lg">Food</button>
+            <button type="button" className="btn btn-outline-secondary btn-lg">Bills</button>
+            <button type="button" className="btn btn-outline-secondary btn-lg">Transport</button>
+            <button type="button" className="btn btn-outline-secondary btn-lg">Healthcare</button>
+            <button type="button" className="btn btn-outline-secondary btn-lg">House</button>
+            <button type="button" className="btn btn-outline-secondary btn-lg">Savings</button>
           </div>
         </div>
         <br/>
-        <div class="d-flex justify-content-center">
-        <button type="button" class="btn btn-success" onClick={handleRedirect}>+Add New Transaction</button>
+        <div className="d-flex justify-content-center">
+        <button type="button" className="btn btn-success" onClick={handleRedirect}>+Add New Transaction</button>
           </div>
       </div>
 
